e4197032c2930ee7c92259d9229e7b6203571714
hafizh hamas m <hafizhhamasm16@gmail.com>
2025-11-30 14:32:42 +0700

    penambahan migration dan model employee

---
+++ app/Models/Employee.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     * Tujuannya agar field ini bisa diisi menggunakan metode create() atau update().
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nama_lengkap',
+        'email',
+        'nomor_telepon',
+        'tanggal_lahir',
+        'alamat',
+        'tanggal_masuk',
+        'status',
+    ];
+}
